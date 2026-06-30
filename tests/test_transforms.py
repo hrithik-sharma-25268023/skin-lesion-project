c08b90ba885b965bfb5eb35bcf0326126cadb694
hrithik-sharma25268023 <25268023@studentmail.ul.ie>
2026-06-30 23:25:05 +0100

    fixed test and CI

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,14 +1,15 @@
 """test cases for train and eval-test transforms"""
 
+from pathlib import Path
 from PIL import Image
 from skin_lesion_project.processing import image_processing
 
 
-
 def test_training_transform_output():
-    """tests the train transforms"""
+    """Tests the training transforms."""
 
-    original = Image.open("/mnt/d/skin-lesion-data/2019+2020/train/ISIC_0000013_2019.jpg").convert("RGB")
+    image_path = Path(__file__).parent / "assets" / "ISIC_0000013_2019.jpg"
+    original = Image.open(image_path).convert("RGB")
     transform = image_processing.training_data_transforms()
     transformed = image_processing.tensor_to_pil(transform(original))
     assert transformed.size == (224, 224)
